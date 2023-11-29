d27972eb441951979e83520bd1a76a33d1012591
pde-rent <paul@astrolab.fi>
2023-11-29 14:30:18 +0100

    updated ISwapper.sol

--- interfaces/ISwapper.sol
+++ interfaces/ISwapper.sol
@@ -5,29 +5,6 @@ pragma solidity ^0.8.17;
 /// @title ISwapper
 /// @dev Interface for Swapper contract to proxy erc20 token swaps through a specified router
 interface ISwapper {
-    // Events
-    event Swapped(
-        address indexed user,
-        address indexed assetIn,
-        address indexed assetOut,
-        uint256 amountIn,
-        uint256 amountOut
-    );
-    event Whitelisted(address indexed account); // caller/asset/router whitelist
-    event RemovedFromWhitelist(address indexed account);
-
-    // Functions to manage restrictions and approvals
-    function setCallerRestriction(bool _restrictCaller) external;
-    function setRouterRestriction(bool _restrictRouter) external;
-    function setApproveMax(bool _approveMax) external;
-    function setAutoRevoke(bool _autoRevoke) external;
-
-    // Functions to manage the whitelist
-    function addToWhitelist(address _address) external;
-    function removeFromWhitelist(address _address) external;
-    function isWhitelisted(address _address) external view returns (bool);
-
-    // Swap functions
     function swap(
         address _input,
         address _output,
@@ -35,15 +12,44 @@ interface ISwapper {
         uint256 _minAmountOut,
         address _targetRouter,
         bytes calldata _callData
-    ) external payable returns (uint256 received);
+    ) external payable returns (uint256 received, uint256 spent);
 
-    // Multi-swap function
+    function decodeAndSwap(
+        address _input,
+        address _output,
+        uint256 _amount,
+        bytes calldata _params
+    ) external returns (uint256 received, uint256 spent);
+
     function multiSwap(
         address[] calldata _inputs,
         address[] calldata _outputs,
         uint256[] calldata _amountsIn,
         uint256[] calldata _minAmountsOut,
         address[] calldata _targetRouters,
-        bytes[] calldata _callDataList
-    ) external returns (uint256[] memory received);
+        bytes[] calldata _params
+    ) external returns (uint256[] memory received, uint256[] memory spent);
+
+    function decodeAndMultiSwap(
+        address[] calldata _inputs,
+        address[] calldata _outputs,
+        uint256[] calldata _amountsIn,
+        bytes[] calldata _params
+    ) external returns (uint256[] memory received, uint256[] memory spent);
+
+    function setCallerRestriction(bool _restrictCaller) external;
+    function setRouterRestriction(bool _restrictRouter) external;
+    function setInputRestriction(bool _inputRestiction) external;
+    function setOutputRestriction(bool _outputRestiction) external;
+    function setApproveMax(bool _approveMax) external;
+    function setAutoRevoke(bool _autoRevoke) external;
+    function addToWhitelist(address _address) external;
+    function removeFromWhitelist(address _address) external;
+    function isWhitelisted(address _address) external view returns (bool);
+    function isCallerRestricted(address _caller) external view returns (bool);
+    function isRouterRestricted(address _router) external view returns (bool);
+    function isInputRestricted(address _input) external view returns (bool);
+    function isOutputRestricted(address _output) external view returns (bool);
+    function isApproveMax() external view returns (bool);
+    function isAutoRevoke() external view returns (bool);
 }
